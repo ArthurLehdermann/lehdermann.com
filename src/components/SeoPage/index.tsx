2f86d6b4665f7602bc85b944ee1b4677dff48670
Arthur Lehdermann <ArthurLehdermann@gmail.com>
2023-04-28 13:24:13 -0300

    SEO

--- src/components/SeoPage/index.tsx
+++ src/components/SeoPage/index.tsx
@@ -10,11 +10,29 @@ export default function SeoPage({ title, description }: SeoPageProps) {
 
   return (
     <NextSeo
-      title={title}
-      description={description}
-      openGraph={{
-        url, title
-      }}
+        title="Arthur Lehdermann"
+        description="Full Stack Web Developer"
+        canonical="https://lehdermann.com/"
+        openGraph={{
+            url: 'https://lehdermann.com/',
+            title: 'Arthur Lehdermann',
+            description: 'Full Stack Web Developer',
+            images: [
+                {
+                    url: 'https://lehdermann.com/ArthurLehdermann.jpeg',
+                    width: 512,
+                    height: 512,
+                    alt: 'Arthur Lehdermann',
+                    type: 'image/jpeg',
+                }
+            ],
+            siteName: 'Arthur Lehdermann',
+        }}
+        twitter={{
+            handle: '@handle',
+            site: '@site',
+            cardType: 'summary_large_image',
+        }}
     />
   );
 }
